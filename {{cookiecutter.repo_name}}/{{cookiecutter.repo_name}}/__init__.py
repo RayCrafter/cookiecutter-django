a4a7d732d9ed75e4ca2e4dd5414c296d411cc7bf
David Zuber <zuber.david@gmx.de>
2015-07-02 11:07:23 +0200

    Ignore unused import of celery

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+from .celery import app as celery_app  # noqa
 
 __version__ = '{{ cookiecutter.version }}'
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
